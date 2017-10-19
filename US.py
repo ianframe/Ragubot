234467e767b372d61e22ae14a6dde79ad0fa864f
ianframe <ian.frame@hies.org>
2017-10-19 08:54:33 -0400

    Missing self reference
    In a Python class, each function requires a reference to `self` as its first argument/parameter.

--- US.py
+++ US.py
@@ -7,7 +7,7 @@ class US:
         self.echo = echo
         this.readings = [128, 128, 128, 128, 128]
 
-    def get_distanc():
+    def get_distanc( ):
         distance = read_ultrasonic_sensor(trig, echo)
         if (distance != null):
             readings.append(distance)
