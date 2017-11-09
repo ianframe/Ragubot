18b69693c1d8f3c1a563eb55699b7b890ebdfb9d
ianframe <ian.frame@hies.org>
2017-11-09 12:27:55 -0500

    Update US.py

--- US.py
+++ US.py
@@ -2,19 +2,18 @@ import pigpio, eyw2
 import numpy as np
 
 class US:
-    def __init__(self, trig, echo):
+    def __init__(self, trig, echo, safe_distance):
         self.trig = trig
         self.echo = echo
+        self.safe_distance = safe_distance
         this.readings = [128, 128, 128, 128, 128]
 
-    def get_distanc():
+    def get_distance():
         distance = read_ultrasonic_sensor(trig, echo)
         if (distance != null):
             readings.append(distance)
             readings.pop(0)
         return np.mean(readings) 
-
-        
 
 
 
